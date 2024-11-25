2462f41e9617613a5d828bd5b63e296e962e5628
wsl_shh <3042569263@qq.com>
2024-11-25 11:01:55 +0800

    add DB

---
+++ DB/mysql/setup_mysql.sh
@@ -0,0 +1,10 @@
+docker run \
+    --platform linux/x86_64 \
+    -p 3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=rootpassword \
+    -e MYSQL_DATABASE=package_server \
+    -e MYSQL_USER=packages_rw \
+    -e MYSQL_PASSWORD=password \
+    -v "$(pwd)/mysql-init":/docker-entrypoint-initdb.d \
+    -ti mysql:8.0.26 \
+    --default-authentication-plugin=mysql_native_password
